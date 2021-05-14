04d901fecc2398278b459cf91c5e83c37adb9141
John Fontaine <john@webware.tech>
2021-05-14 18:03:18 +0200

    Added api endpoints

--- src/index.js
+++ src/index.js
@@ -5,8 +5,23 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
+app.get('/home', (req, res) => {
   res.send({ message: 'Hello' });
+});
+
+app.get('/page/:id', (req, res) => {
+  res.send({ id: req.params.id });
+});
+
+app.get('/security/:id', (req, res) => {
+  setTimeout(
+    () =>
+      res.send({
+        view: ['SEARCH', 'GRID', 'ADD'],
+        access: req.params.id === '1' ? ['EDIT', 'DELETE', 'VIEW', 'ADD'] : ['VIEW'],
+      }),
+    Math.floor(Math.random() * 2000 + 100)
+  );
 });
 
 app.listen(3000, () => {
